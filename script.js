33c6d80139df9ec0c6ad46197ddf1d2edf3f5a66
Coutoedu5-byte <coutoedu5@gmail.com>
2026-05-27 10:42:25 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let playerName = "";
 let roomCode = "";
 
 function createRoom() {
-    playerName = document.getElementById("playerName").value;
+
+    playerName =
+        document.getElementById("playerName").value;
 
     if (!playerName) {
         alert("Digite seu nome");
@@ -15,14 +17,17 @@ function createRoom() {
 }
 
 function joinRoom() {
-    playerName = document.getElementById("playerName").value;
+
+    playerName =
+        document.getElementById("playerName").value;
 
     if (!playerName) {
         alert("Digite seu nome");
         return;
     }
 
-    roomCode = prompt("Digite o código da sala");
+    roomCode =
+        prompt("Digite o código da sala");
 
     if (!roomCode) return;
 
@@ -33,30 +38,75 @@ function joinRoom() {
 }
 
 socket.on("roomCreated", (code) => {
+
     roomCode = code;
 
-    document.body.innerHTML = `
-        <div style="padding:20px;color:white">
-            <h1>Sala criada</h1>
-            <h2>Código: ${code}</h2>
-            <p>Aguardando jogadores...</p>
-        </div>
-    `;
+    document.getElementById("menuScreen")
+        .classList.add("hidden");
+
+    document.getElementById("lobbyScreen")
+        .classList.remove("hidden");
+
+    document.getElementById("roomCode")
+        .innerText = code;
+
+    updatePlayers([playerName]);
 });
 
 socket.on("roomJoined", (players) => {
-    document.body.innerHTML = `
-        <div style="padding:20px;color:white">
-            <h1>Sala: ${roomCode}</h1>
-            <h2>Jogadores:</h2>
-            ${players.map(p => `<p>${p}</p>`).join("")}
-        </div>
-    `;
+
+    document.getElementById("menuScreen")
+        .classList.add("hidden");
+
+    document.getElementById("lobbyScreen")
+        .classList.remove("hidden");
+
+    updatePlayers(players);
+});
+
+function updatePlayers(players){
+
+    const playersList =
+        document.getElementById("players");
+
+    playersList.innerHTML = "";
+
+    players.forEach(player => {
+
+        const li =
+            document.createElement("li");
+
+        li.innerText = player;
+
+        playersList.appendChild(li);
+    });
+}
+
+socket.on("updatePlayers", (players) => {
+    updatePlayers(players);
 });
 
 socket.on("errorMessage", (msg) => {
     alert(msg);
 });
+
 function startGame(){
+
     alert("Partida iniciada!");
+
+    document.getElementById("roleScreen")
+        .classList.remove("hidden");
+
+    document.getElementById("roleTitle")
+        .innerText = "CIDADÃO";
+
+    document.getElementById("roleDescription")
+        .innerText =
+            "Descubra quem é o assassino.";
+}
+
+function closeRole(){
+
+    document.getElementById("roleScreen")
+        .classList.add("hidden");
 }
