aecf2a3d75a353ce10f5a95edfbe8c97de43a6e0
Coutoedu5-byte <coutoedu5@gmail.com>
2026-05-27 10:03:40 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,140 +1,52 @@
 const express = require("express");
 const app = express();
-
 const http = require("http").createServer(app);
-
 const io = require("socket.io")(http);
 
-app.use(express.static("./"));
+app.use(express.static(__dirname));
 
 const rooms = {};
 
-function randomRole() {
-
-    const roles = [
-        "Assassino",
-        "Detetive",
-        "Médico",
-        "Cidadão"
-    ];
-
-    return roles[Math.floor(Math.random() * roles.length)];
+function generateCode() {
+    return Math.random().toString(36).substring(2, 7).toUpperCase();
 }
 
 io.on("connection", (socket) => {
 
-    socket.on("createRoom", (name) => {
+    socket.on("createRoom", (playerName) => {
 
-        const room = Math.random()
-            .toString(36)
-            .substring(2, 7);
+        const code = generateCode();
 
-        rooms[room] = {
-            players: []
+        rooms[code] = {
+            players: [playerName]
         };
 
-        rooms[room].players.push({
-            id: socket.id,
-            name,
-            dead: false
-        });
+        socket.join(code);
 
-        socket.join(room);
+        socket.roomCode = code;
 
-        socket.emit("roomCreated", room);
-
-        io.to(room).emit(
-            "updatePlayers",
-            rooms[room].players
-        );
+        io.to(socket.id).emit("roomCreated", code);
     });
 
-    socket.on("joinRoom", (data) => {
+    socket.on("joinRoom", ({ roomCode, playerName }) => {
 
-        if(!rooms[data.room]) {
+        if (!rooms[roomCode]) {
+            io.to(socket.id).emit("errorMessage", "Sala não encontrada");
             return;
         }
 
-        rooms[data.room].players.push({
-            id: socket.id,
-            name: data.name,
-            dead: false
-        });
+        rooms[roomCode].players.push(playerName);
 
-        socket.join(data.room);
+        socket.join(roomCode);
 
-        socket.emit("roomJoined", {
-            room: data.room,
-            players: rooms[data.room].players
-        });
-
-        io.to(data.room).emit(
-            "updatePlayers",
-            rooms[data.room].players
+        io.to(roomCode).emit(
+            "roomJoined",
+            rooms[roomCode].players
         );
     });
 
-    socket.on("chat", (data) => {
-        io.to(data.room).emit("chat", data);
-    });
-
-    socket.on("startGame", (room) => {
-
-        const players = rooms[room].players;
-
-        players.forEach((player) => {
-
-            let role = randomRole();
-
-            io.to(player.id).emit("role", role);
-        });
-    });
-
-    socket.on("vote", (data) => {
-
-        const room = rooms[data.room];
-
-        if(!room) return;
-
-        const target = room.players.find(
-            p => p.name === data.target
-        );
-
-        if(target) {
-
-            target.dead = true;
-
-            io.to(data.room).emit(
-                "playerEliminated",
-                target.name
-            );
-
-            io.to(data.room).emit(
-                "updatePlayers",
-                room.players
-            );
-        }
-    });
-
-    socket.on("disconnect", () => {
-
-        for(const room in rooms) {
-
-            rooms[room].players =
-                rooms[room].players.filter(
-                    p => p.id !== socket.id
-                );
-
-            io.to(room).emit(
-                "updatePlayers",
-                rooms[room].players
-            );
-        }
-    });
 });
 
-const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log("Servidor rodando");
 });
