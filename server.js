84aed9d3b0b411bc313bceb91b428fed69cfdfa8
Coutoedu5-byte <coutoedu5@gmail.com>
2026-05-27 14:05:42 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,52 +1,180 @@
 const express = require("express");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+
+const http =
+require("http").createServer(app);
+
+const io =
+require("socket.io")(http);
 
 app.use(express.static(__dirname));
 
 const rooms = {};
 
-function generateCode() {
-    return Math.random().toString(36).substring(2, 7).toUpperCase();
+function generateCode(){
+
+    return Math.random()
+        .toString(36)
+        .substring(2,7)
+        .toUpperCase();
+}
+
+/* EMBARALHAR */
+function shuffle(array){
+
+    for(let i = array.length - 1; i > 0; i--){
+
+        const j =
+            Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] =
+            [array[j], array[i]];
+    }
+
+    return array;
 }
 
 io.on("connection", (socket) => {
 
+    /* CRIAR SALA */
     socket.on("createRoom", (playerName) => {
 
         const code = generateCode();
 
         rooms[code] = {
-            players: [playerName]
+            players: []
         };
+
+        rooms[code].players.push({
+            id: socket.id,
+            name: playerName
+        });
 
         socket.join(code);
 
         socket.roomCode = code;
 
-        io.to(socket.id).emit("roomCreated", code);
+        io.to(socket.id)
+            .emit("roomCreated", code);
     });
 
-    socket.on("joinRoom", ({ roomCode, playerName }) => {
+    /* ENTRAR SALA */
+    socket.on("joinRoom", ({
+        roomCode,
+        playerName
+    }) => {
 
-        if (!rooms[roomCode]) {
-            io.to(socket.id).emit("errorMessage", "Sala não encontrada");
+        if(!rooms[roomCode]){
+
+            io.to(socket.id)
+                .emit(
+                    "errorMessage",
+                    "Sala não encontrada"
+                );
+
             return;
         }
 
-        rooms[roomCode].players.push(playerName);
+        rooms[roomCode].players.push({
+            id: socket.id,
+            name: playerName
+        });
 
         socket.join(roomCode);
 
-        io.to(roomCode).emit(
-            "roomJoined",
-            rooms[roomCode].players
-        );
+        socket.roomCode = roomCode;
+
+        io.to(roomCode)
+            .emit(
+                "updatePlayers",
+                rooms[roomCode].players.map(
+                    p => p.name
+                )
+            );
+    });
+
+    /* INICIAR PARTIDA */
+    socket.on("startGame", () => {
+
+        const roomCode =
+            socket.roomCode;
+
+        const room =
+            rooms[roomCode];
+
+        if(!room) return;
+
+        const totalPlayers =
+            room.players.length;
+
+        /* MÍNIMO */
+        if(totalPlayers < 4){
+
+            io.to(socket.id)
+                .emit(
+                    "errorMessage",
+                    "Mínimo de 4 jogadores"
+                );
+
+            return;
+        }
+
+        /* CARGOS FIXOS */
+        const roles = [
+
+            "assassino",
+            "assassino",
+            "medico",
+            "detetive"
+
+        ];
+
+        /* RESTO = CIDADÃO */
+        while(roles.length < totalPlayers){
+
+            roles.push("cidadao");
+        }
+
+        /* EMBARALHA */
+        shuffle(roles);
+
+        /* ENTREGA CARTAS */
+        room.players.forEach((player, index) => {
+
+            io.to(player.id)
+                .emit(
+                    "receiveRole",
+                    roles[index]
+                );
+        });
+    });
+
+    /* DESCONECTAR */
+    socket.on("disconnect", () => {
+
+        for(const code in rooms){
+
+            rooms[code].players =
+                rooms[code].players.filter(
+                    p => p.id !== socket.id
+                );
+
+            io.to(code)
+                .emit(
+                    "updatePlayers",
+                    rooms[code].players.map(
+                        p => p.name
+                    )
+                );
+        }
     });
 
 });
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor rodando");
-});
+http.listen(
+    process.env.PORT || 3000,
+    () => {
+        console.log("Servidor rodando");
+    }
+);
